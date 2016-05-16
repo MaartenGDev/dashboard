b84c668fab8ad5ba66e91b96371071ab142b9c71
Maarten <maarten@advancedcoding.nl>
2016-05-16 22:29:47 +0200

    added extra stats row for general stats

--- resources/assets/js/collapsible.js
+++ resources/assets/js/collapsible.js
@@ -1,8 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapsible-header').click(function () {
         var dropdownContent = $(this).parents('.collapsible').find('.collapsible-body');
         var currentContent = $(this).parent().find('.collapsible-body');
+        var isHidden = currentContent.hasClass('hidden');
         dropdownContent.addClass('hidden');
-        currentContent.removeClass('hidden');
+
+        if(isHidden) currentContent.removeClass('hidden');
+
     });
 });
